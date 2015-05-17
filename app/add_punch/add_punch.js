25745c692f80e4e6aeb7c2f4c598b5cd87f57a3e
Roselma Mendes <roselma.mendes@gmail.com>
2015-05-17 18:14:34 -0300

    Part of the bootstrap and make a first test.

--- app/add_punch/add_punch.js
+++ app/add_punch/add_punch.js
@@ -1,14 +1,17 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.add_punch', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+  $routeProvider.when('/add_punch', {
+    templateUrl: 'add_punch/add_punch.html',
+    controller: 'AddPunchCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+    .controller('AddPunchCtrl', function($scope) {
+	$scope.work_days = function(){
+	    return [{start: new Date(2015,4,17,10,0), end: new Date(2015,4,17,19,0)}];
+	};
+	
+});
--- app/add_punch/add_punch_test.js
+++ app/add_punch/add_punch_test.js
@@ -6,10 +6,15 @@ describe('myApp.add_punch module', function() {
 
   describe('add_punch controller', function(){
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+      it('should receive a start and end', inject(function($controller) {
+	  var $scope = {};
+	  var addPunchCtrl = $controller('AddPunchCtrl',{$scope:$scope});
+	  $scope.start = "10:00AM";
+	  var start_date = new Date(2015,4,17,10,0);
+	  var end_date = new Date(2015,4,17,19,0)
+	  var expected_work_days = [{start: start_date, end: end_date}]
+	  
+	  expect(expected_work_days).toEqual($scope.work_days());
     }));
 
   });
