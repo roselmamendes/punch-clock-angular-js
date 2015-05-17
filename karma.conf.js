863e8d67b7918356d43609055f3fb504eafddfe9
Roselma Mendes <roselma.mendes@gmail.com>
2015-05-17 11:40:08 -0300

    Modify setting files.

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,84 @@
-module.exports = function(config){
+// Karma configuration
+// Generated on Tue May 12 2015 22:43:28 GMT-0300 (BRT)
+
+module.exports = function(config) {
   config.set({
 
-    basePath : './',
+    // base path, that will be used to resolve files and exclude
+    basePath: './',
 
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
       'app/view*/**/*.js'
+    ], 
+
+
+    // list of files to exclude
+    exclude: [
+      
     ],
 
-    autoWatch : true,
 
-    frameworks: ['jasmine'],
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
-    browsers : ['Chrome'],
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],
 
     plugins : [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
-            'karma-junit-reporter'
+            'karma-junit-reporter',
+ 	    'karma-phantomjs-launcher'
             ],
 
     junitReporter : {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
-    }
+    },
+      
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
   });
 };
