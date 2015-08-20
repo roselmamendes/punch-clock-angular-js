af25cea0f5c50e758c26ec812248a9e3c1be1f3d
Roselma Mendes <roselma.mendes@gmail.com>
2015-08-20 13:50:56 -0300

    Code done at dojo.

--- app/punch/punch-controller_test.js
+++ app/punch/punch-controller_test.js
@@ -7,7 +7,49 @@ describe('Punch Controller', function(){
     }));
     
     it('Add a punch', function(){
-        controller.add_punch('17/08/2015','08:00');
-        expect(controller.getAll()).toBe([{'17/08/2015':['08:00']}]);
+        var scope = {};
+        controller('PunchController', {$scope: scope});
+        scope.addPunch('17/08/2015', '08:00');
+        expect(scope.getAll()).toEqual(
+            [{date: '17/08/2015', time: '08:00'}]);
     });
+    
+    it('Get all punches', function(){
+        var scope = {};
+        controller('PunchController', {$scope: scope});
+        scope.addPunch('17/08/2015', '08:00');
+        scope.addPunch('17/08/2015', '18:00');
+        expect(scope.getAll()).toEqual(
+            [{date: '17/08/2015', time: '08:00'},
+             {date: '17/08/2015', time: '18:00'}]);
+    });
+    
+    it('get punch by day', function() {
+        var scope = {};
+        controller('PunchController', {$scope: scope});
+        scope.addPunch('17/08/2015', '08:00');
+        scope.addPunch('18/08/2015', '18:00');
+        expect(scope.getPunchesByDay('17/08/2015')).toEqual(
+            [
+                {date: '17/08/2015', time: '08:00'}
+            ]
+        );
+    });
+    
+    it('get punches by day', function(){
+        var scope = {};
+        controller('PunchController', {$scope: scope});
+        scope.addPunch('17/08/2015', '08:00');
+        scope.addPunch('17/08/2015', '18:00');
+        scope.addPunch('18/08/2015', '08:00');
+        scope.addPunch('18/08/2015', '18:00');
+        expect(scope.getPunchesByDay('18/08/2015')).toEqual(
+            [
+                {date: '18/08/2015', time: '08:00'},
+                {date: '18/08/2015', time: '18:00'}
+            ]
+        );
+    });
+    
+    
 });
--- app/punch/punch.js
+++ app/punch/punch.js
@@ -1,1 +1,22 @@
-angular.module('PunchApp',[]);
+angular.module('PunchApp',[]).controller("PunchController",
+function($scope) {
+    var punches = [];
+    $scope.addPunch = function(date, time) {
+        punches.push({date: date, time: time});
+    };
+    
+    $scope.getAll = function() {
+        return punches;
+    };
+    
+    $scope.getPunchesByDay = function(date) {
+        return punches.filter(function(element){
+            return element.date == date;
+        });
+    };
+    
+    $scope.getPunchesBetween = function(startDate, endDate) {
+        //TODO
+    };
+});
+
